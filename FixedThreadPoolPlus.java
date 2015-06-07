ea2790226a9aefd271c19a7d928348307d32e936
yuekuo <yuekuo.lq@alibaba-inc.com>
2015-06-07 13:18:58 +0800

    完善注释信息

--- FixedThreadPoolPlus.java
+++ FixedThreadPoolPlus.java
@@ -21,10 +21,16 @@ import java.util.concurrent.TimeUnit;
 import java.util.concurrent.atomic.AtomicInteger;
 
 /**
- * 对java线程池FixedThreadPool的加强，支持在线程池中线程耗尽的情况下，将线程池中所有线程当前正在执行那条代码的信息汇总并打印出来，
- * 同时将全部的完整信息保存到用户目录下的 "线程名.detail" 文件中， 方便定位及排查线程池耗尽时的现场环境 目前只支持Linux
- * 
- * @author yuekuo.liq
+* 支持在线程池中线程耗尽的情况下，将线程池中所有线程当前正在执行那条代码的信息汇总，作为异常信息抛出，
+* 同时将线程全部的完整堆栈信息保存到用户目录下的 "线程名.detail" 文件中，方便定位及排查线程池耗尽时的现场环境，目前只支持Linux
+* 信息包含类似如下的提示(如果是10个线程的线程池):
+* 
+* Exception in thread "main" java.util.concurrent.RejectedExecutionException: Thread pool is EXHAUSTED! Thread Name: test-thredpool-plus, Pool Size: 10 (active: 10, core: 10, max: 10, largest: 10), Task: 10 (completed: 0), Executor status:(isShutdown:false, isTerminated:false, isTerminating:false)
+* 6 threads are hanging on this code : at java.net.PlainSocketImpl.socketConnect(Native Method)
+* 4 threads are hanging on this code : at java.lang.Thread.sleep(Native Method)
+* You can fetch more thread statck information by the detail file : /Users/yuekuo/test-thredpool-plus.detail
+*
+* @author yuekuo.liq
  */
 public class FixedThreadPoolPlus {
     /**
